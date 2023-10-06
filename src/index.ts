98bc8c621c1f8955812adc8c14bdc40094b2de55
Dastan Abdraym <dastan.abdiraiym@bd.qatarinsurance.com>
2023-10-06 16:42:18 +0600

    add library build

---
+++ src/index.ts
@@ -0,0 +1,2 @@
+export { default as ChocoMiniappDebugbar } from './components/MiniappDebugbar.vue';
+export * from './shared/composables';
--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,14 @@
 import { defineConfig } from 'vite';
 import { resolve } from 'path';
 import vue from '@vitejs/plugin-vue';
+import dts from 'vite-plugin-dts';
 
 // https://vitejs.dev/config/
 export default defineConfig({
 	server: {
 		port: 8080
 	},
-	plugins: [vue()],
+	plugins: [vue(), dts()],
 	resolve: {
 		extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json', '.vue'],
 		alias: [
@@ -16,5 +17,20 @@ export default defineConfig({
 				replacement: resolve(__dirname, './src')
 			}
 		]
+	},
+	build: {
+		lib: {
+			entry: resolve(__dirname, './src/index.ts'),
+			name: 'ChocoMiniappDebugbar',
+			fileName: 'choco-miniapp-debugbar'
+		},
+		rollupOptions: {
+			external: ['vue'],
+			output: {
+				globals: {
+					vue: 'Vue'
+				}
+			}
+		}
 	}
 });
